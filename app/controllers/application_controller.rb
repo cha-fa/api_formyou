fc977d3ed7b3c43b6696dfb9b4951765cef26c1d
cha-fa <favier.cha@gmail.com>
2021-03-08 16:05:55 +0100

    Fix protext forgery, add routes users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :null_session
 
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def render_resource(resource)
