710ac650ba83307f53396152e4e4c96da3064a76
Quentin PLAUD <qplaud@gmail.com>
2021-03-08 19:18:54 +0100

    Quick fix done, no more irrelevant warning message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
-  protect_from_forgery with: :null_session
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
