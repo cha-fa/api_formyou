41bcc751266d30b270ad4d9f79dc849201e230c2
Quentin PLAUD <qplaud@gmail.com>
2021-03-08 19:35:54 +0100

    Delete flash middleware

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,5 @@ module THPgramApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.use ActionDispatch::Flash
   end
 end
