3a79ad397fe71dc3eb6cb0854dfd679ee4f55917
Querne <jeremyquerne@hotmail.fr>
2021-03-10 19:54:47 +0100

    test export category and courses

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1
   def show
-    render json: @category
+    render json: {category: @category, courses: @category.courses}
   end
 
   # POST /categories
